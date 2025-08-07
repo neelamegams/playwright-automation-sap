dd25d43a5a563f9c0bc46ca1f7d3a650cd1c911b
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2025-08-07 20:16:45 +0530

    Added v2 version for role collections addition

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -143,6 +143,86 @@ test('Delete CF:Space Developer Role', async ({ page }) => {
     }
 });
 
+test('-V2-Add CF: Role Collections', async ( {page}) => {
+  //Gen AI CodeJam Tile
+  await page.goto('https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/275320f9-4c26-4622-8728-b6f5196075f5/subaccount/a5a420d8-58c6-4820-ab11-90c7145da589/rolecollections');
+  await page.locator('button:text("Accept all")').click();
+
+  //await page.locator('#__xmlview0--roleCollectionsList-triggerList').click();
+
+  //Click on the More button to load more role collections
+  const growingListTrigger = page.locator('div.sapMGrowingListTrigger');    
+  await growingListTrigger.waitFor({ state: 'visible' });    
+  await growingListTrigger.click();
+  
+  //Look for row containing 'CodeJam' and click on the arrow icon
+  const CodeJamRoleCollectionRow = await page.getByRole('row').filter({ hasText: 'CodeJam' });
+  await CodeJamRoleCollectionRow.waitFor({ state: 'visible' });
+
+  const navIcon = CodeJamRoleCollectionRow.locator("td").nth(6);
+  await navIcon.waitFor({ state: 'visible' });
+  await navIcon.click();
+
+  //Added this line to ensure the page is fully loaded before clicking on the Edit button
+  await page.waitForSelector('#__xmlview3');
+
+  const EditButton = await page.getByRole('button', { name: 'Edit' });
+  await EditButton.waitFor({ state: 'visible' });
+  await EditButton.click();
+
+  const UsersTab = await page.getByRole('tab', { name: 'Users' });
+  await UsersTab.waitFor({ state: 'visible' });
+  await UsersTab.click();
+  
+  //Read emails from a file and paste them in the input field
+  const filePath = './tests/lib/emails.txt'; // Replace with your file path
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  
+  const lines = fileContent.split('\n').map(line => line.trim()).filter(line => line.length > 0);
+  let index = 0;
+
+  // Example: log each line
+  for (const line of lines) {
+    console.log("Processing user : " + line);
+   
+    const inputIDField = await page.locator('table#__xmlview3--rolecollectionsDetailUsersList-listUl td input').first();
+    await inputIDField.waitFor({ state: 'visible' });
+    await inputIDField.fill(line);
+
+    if(index == 0){
+      const idpField = await page.locator('table#__xmlview3--rolecollectionsDetailUsersList-listUl tr').nth(1).locator("td").nth(2).click();
+      await page.locator('ul[role="listbox"]').getByText('Default identity provider').nth(0).click();
+      await page.locator('table#__xmlview3--rolecollectionsDetailUsersList-listUl tr').nth(1).locator("td").nth(3).locator("input").fill(line); // Simulate typing
+      
+      const SaveButton = await page.getByRole('button', { name: 'Save' });
+      await SaveButton.waitFor({ state: 'visible' });
+      await SaveButton.click();
+      console.log("Added user - if flow: " + line);
+    }
+    else{
+        await page.locator('table#__xmlview3--rolecollectionsDetailUsersList-listUl tr').nth(1).locator("td").nth(2).click();
+        const idpField = await page.locator('ul[role="listbox"] li:has-text("Default identity provider")').first();
+        await idpField.waitFor({ state: 'visible' });
+        await idpField.click();
+
+        const EmailField = await page.locator('table#__xmlview3--rolecollectionsDetailUsersList-listUl tr').nth(1).locator("td").nth(3).locator('input');
+        await EmailField.waitFor({ state: 'visible' });
+        await EmailField.fill(line);
+
+        const SaveButton = await page.getByRole('button', { name: 'Save' });
+        await SaveButton.waitFor({ state: 'visible' });
+        await SaveButton.click();
+        console.log("Added user - else flow : " + line);
+    }
+      const EditButton = await page.getByRole('button', { name: 'Edit' });
+      await EditButton.waitFor({ state: 'visible' });
+      await EditButton.click();
+  
+      index++;
+  }
+
+});
+
 test('Add CF:Role Collections', async ( {page}) => {
   //Gen AI CodeJam Tile
   //Security >> CodeJamParticipant Role >> Add email ids
@@ -210,27 +290,19 @@ test('Add CF:Role Collections', async ( {page}) => {
       const EditButton = await page.getByRole('button', { name: 'Edit' });
       await EditButton.waitFor({ state: 'visible' });
       await EditButton.click();
-      
-      // const AddUserPlusButton = await page.getByRole('button', { name: 'Add a user' }).first();
-      // await AddUserPlusButton.waitFor({ state: 'visible' });
-      // await AddUserPlusButton.click();
-
-    
-
-    index++;
+  
+      index++;
   }
-
-
-   
 
 });
 
 
 test('Delete CF:Role Collections', async ( {page}) => {
   //CAP CodeJam Tile
+  //https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/275320f9-4c26-4622-8728-b6f5196075f5/subaccount/13f4f274-4515-4c67-8274-cbde80a4e744/rolecollections
   //Security >> CodeJamParticipant Role >> Add email ids
-
-  await page.goto('https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/275320f9-4c26-4622-8728-b6f5196075f5/subaccount/13f4f274-4515-4c67-8274-cbde80a4e744/rolecollections');
+  //Gen AI CodeJam Tile
+  await page.goto('https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/275320f9-4c26-4622-8728-b6f5196075f5/subaccount/a5a420d8-58c6-4820-ab11-90c7145da589/rolecollections');
   await page.locator('button:text("Accept all")').click();
 
   const CodeJamRoleCollectionRow = await page.getByRole('row').filter({ hasText: 'CodeJam' }).locator('#__item9-__clone4-TypeCell')
@@ -241,7 +313,7 @@ test('Delete CF:Role Collections', async ( {page}) => {
   await UsersTab.waitFor({ state: 'visible' });
   await UsersTab.click();
 
-  await page.waitForResponse('https://emea.cockpit.btp.cloud.sap/ajax/275320f9-4c26-4622-8728-b6f5196075f5/cf-us10/13f4f274-4515-4c67-8274-cbde80a4e744/getRoleCollectionUsersCall/13f4f274-4515-4c67-8274-cbde80a4e744/loadRoleCollectionDetailUsers?rolecollection=CodeJam');
+  //await page.waitForResponse('https://emea.cockpit.btp.cloud.sap/ajax/275320f9-4c26-4622-8728-b6f5196075f5/cf-us10/13f4f274-4515-4c67-8274-cbde80a4e744/getRoleCollectionUsersCall/13f4f274-4515-4c67-8274-cbde80a4e744/loadRoleCollectionDetailUsers?rolecollection=CodeJam');
 
   const EditButton = await page.getByRole('button', { name: 'Edit' });
   await EditButton.waitFor({ state: 'visible' });
@@ -264,7 +336,6 @@ test('Delete CF:Role Collections', async ( {page}) => {
       
     }
 
-
     const SaveButton = await page.getByRole('button', { name: 'Save' });
     await SaveButton.waitFor({ state: 'visible' });
     await SaveButton.click();
@@ -280,7 +351,7 @@ test('Create Users from Security > Users', async ( {page}) => {
   //Read emails from a file and paste them in the input field
   const filePath = './tests/lib/emails.txt'; // Replace with your file path
   const fileContent = fs.readFileSync(filePath, 'utf-8');
-  console.log(fileContent);
+  //console.log(fileContent);
   const lines = fileContent.split('\n').map(line => line.trim()).filter(line => line.length > 0);
 
   for (const line of lines) {
@@ -291,7 +362,7 @@ test('Create Users from Security > Users', async ( {page}) => {
     // Use type() to simulate real typing (fires all events)
     await page.locator(inputSelector).fill(line); // Simulate typing
     //await page.keyboard.press('Tab', { delay: 1000 }); // Move to the next field
-    console.log(line);
+    console.log("Creating :" + line);
     await page.waitForResponse('https://emea.cockpit.btp.cloud.sap/ajax/275320f9-4c26-4622-8728-b6f5196075f5/cf-eu10-004/a5a420d8-58c6-4820-ab11-90c7145da589/getShadowUsersCall/a5a420d8-58c6-4820-ab11-90c7145da589/'+line, { timeout: 5000 });
     await page.locator(inputSelector).evaluate((el) => el.blur());
    
@@ -316,6 +387,7 @@ test('Create Users from Security > Users', async ( {page}) => {
     const createButton = await page.locator('.sapMDialogFooter').getByRole('button', { name: 'Create' }).first();
     await createButton.waitFor({ state: 'visible' });
     await createButton.click();
+    console.log("Created :" + line);
   }
 
 });
@@ -323,9 +395,9 @@ test('Create Users from Security > Users', async ( {page}) => {
 
 test('Delete Users from Security > Users', async ( {page}) => {
   //CAP CodeJam url
-  await page.goto('https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/275320f9-4c26-4622-8728-b6f5196075f5/subaccount/13f4f274-4515-4c67-8274-cbde80a4e744/usersOverview&//');
+  //await page.goto('https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/275320f9-4c26-4622-8728-b6f5196075f5/subaccount/13f4f274-4515-4c67-8274-cbde80a4e744/usersOverview&//');
   //Gen AI CodeJam url
-  //await page.goto('https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/275320f9-4c26-4622-8728-b6f5196075f5/subaccount/a5a420d8-58c6-4820-ab11-90c7145da589/usersOverview&//');
+  await page.goto('https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/275320f9-4c26-4622-8728-b6f5196075f5/subaccount/a5a420d8-58c6-4820-ab11-90c7145da589/usersOverview&//');
   await page.locator('button:text("Accept all")').click();
 
   //Read emails from a file and paste them in the input field
@@ -334,11 +406,9 @@ test('Delete Users from Security > Users', async ( {page}) => {
   //console.log(fileContent);
   const lines = fileContent.split('\n').map(line => line.trim()).filter(line => line.length > 0);
 
-
-
   for (const line of lines) {
     const moreLink = page.locator('#__xmlview1--usersTable-trigger-content');
-    if(await moreLink.isVisible()){
+    while(await moreLink.isVisible()){
       await moreLink.click();
     }
     await page.getByRole("row").filter({hasText: line}).locator('button', {hasText:'Delete user'}).click();
@@ -348,4 +418,50 @@ test('Delete Users from Security > Users', async ( {page}) => {
     await RemoveButton.click();
     console.log("Deleted user : " + line);
   }
+});
+
+
+test('-V2-Delete Users from Security > Users', async ( {page}) => {
+  //CAP CodeJam url
+  //await page.goto('https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/275320f9-4c26-4622-8728-b6f5196075f5/subaccount/13f4f274-4515-4c67-8274-cbde80a4e744/usersOverview&//');
+  //Gen AI CodeJam url
+  await page.goto('https://emea.cockpit.btp.cloud.sap/cockpit/#/globalaccount/275320f9-4c26-4622-8728-b6f5196075f5/subaccount/a5a420d8-58c6-4820-ab11-90c7145da589/usersOverview&//');
+  await page.locator('button:text("Accept all")').click();
+
+  //Read emails from a file and paste them in the input field
+  const filePath = './tests/lib/emails.txt'; // Replace with your file path
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  //console.log(fileContent);
+  const lines = fileContent.split('\n').map(line => line.trim()).filter(line => line.length > 0);
+  
+  let countDeletedUsers = 0;
+  //Click on the Select multiple users button
+  await page.getByRole('button', { name: 'Select multiple users' }).click();
+
+  //Click on more link to load all users
+  const moreLink = page.locator('div.sapMGrowingListTrigger');
+  await moreLink.waitFor({ state: 'visible' });
+  while(await moreLink.isVisible()){
+    await moreLink.click();
+  }
+
+  for (const line of lines) {
+    const userRow = page.getByRole("row").filter({hasText: line});
+    if(await userRow.isVisible()){
+      const checkbox = userRow.getByRole('checkbox');
+      await checkbox.check();
+      console.log("Selected user : " + line);
+      countDeletedUsers++;
+    }
+    else{
+      console.log("User not found or already deleted: " + line);
+      continue;
+    }
+  }
+
+  await page.getByRole('toolbar').getByRole('button', { name: 'Delete' }).click();
+  
+  //Confirm Delete in the pop up dialog
+  await page.locator("#confirmDelete").locator('button:has-text("Delete")').click();
+  console.log("Deleted " + countDeletedUsers + " users");
 });
