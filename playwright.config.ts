9404fb4d4e21d306825f404158b38ca20a742a40
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2026-01-14 12:41:37 +0530

    added global timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  // Maximum time for the entire test run (10 minutes)
+  globalTimeout: 600000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
